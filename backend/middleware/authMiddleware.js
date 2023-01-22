a433ee5b8fd85897f2c8e934eba914d241f92d45
Joshua Atanu <atanu.joshua@gmail.com>
2023-01-22 15:09:50 +0100

    finished backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,34 +1,36 @@
-const jwt = require("jsonwebtoken");
-const asyncHandler = require("express-async-handler");
-const User = require("../model/userModel");
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  let token
+
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      //get token from header
-      token = req.headers.authorization.split(" ")[1];
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
 
-      // verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      //  get user from token
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-      req.user = await User.findById(decoded.id).select("-password");
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
 
-      next();
+      next()
     } catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error("not authorized");
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized')
     }
   }
+
   if (!token) {
     res.status(401)
-    throw new Error("Not authorized, no token");
+    throw new Error('Not authorized, no token')
   }
-});
+})
 
-module.exports = { protect };
+module.exports = { protect }
