429b171e14e71dae7dfe248a5cae0cb247f95dd0
Joshua Atanu <atanu.joshua@gmail.com>
2023-03-06 16:35:45 +0100

    fix the errar rigister auth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import {FaSignOutAlt ,FaSignInAlt, FaUser} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 function Header() {
+    
   return (
     <header className='header'>
         <div className='logo'>
